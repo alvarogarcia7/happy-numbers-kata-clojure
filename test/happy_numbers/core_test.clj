541879673c015bcdf54cedb47e741e7ccf08f95b
alvaro garcia <derecerca@gmail.com>
2015-02-11 07:10:59 +0100

    add a function for searching in a haystack

---
+++ test/happy_numbers/core_test.clj
@@ -0,0 +1,18 @@
+(ns happy-numbers.core-test
+  (:require [clojure.test :refer :all]
+            [happy-numbers.core :refer :all]))
+
+(defn my-contains [needle haystack]
+  (or
+   (some #(= needle %) haystack)
+   false))
+
+
+(some #(= % 1) '(1))
+; true
+
+(some #(= % 2) '(1))
+; nil
+
+(my-contains 2 [1])
+
